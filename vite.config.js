78585e9992521a8e010622cd46cb422d59bacea5
geoenterprise <jovannyrey@hotmail.com>
2024-10-19 21:31:13 -0500

    basic fanhub structure

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  root: "src/",
+
+  build: {
+    outDir: "../dist",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+      },
+    },
+  },
+});
